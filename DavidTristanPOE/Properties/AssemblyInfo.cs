8d4314659f2f71946e41ac52860bb98933a43912
DavidBarker05 <davidbarker9611@gmail.com>
2024-04-19 16:27:27 +0200

    rename again

--- DavidTristanPOE/Properties/AssemblyInfo.cs
+++ DavidTristanPOE/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DavidTristanICETask3")]
+[assembly: AssemblyTitle("DavidTristanPOE")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DavidTristanICETask3")]
+[assembly: AssemblyProduct("DavidTristanPOE")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
