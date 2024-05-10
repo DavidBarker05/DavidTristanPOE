6d14d3c93be2ec8e3c230487e97ca2bbd3664d49
DavidBarker05 <davidbarker9611@gmail.com>
2024-05-10 11:30:20 +0200

    Changed from method to lambda, and added reference for delegate with lambda

--- DavidTristanPOE/TurnPlayerForm.cs
+++ DavidTristanPOE/TurnPlayerForm.cs
@@ -6,26 +6,31 @@ namespace DavidTristanPOE
 {
     public partial class TurnPlayerForm : Form
     {
+
         public TurnPlayerForm()
         {
             InitializeComponent();
             btnRest.Visible = false;
             /*
-             * Note: This is an in-text citation (for url see the reference list in References.Docx)
+             * NOTE: These are in-text citations (for urls see the reference list in README/References.Docx)
              * 
-             * Closing the second form wouldn't exit the application, so we needed to force an exit while it was closing, Microsoft ([s.a.]) demonstrates how to access this moment using the Form.FormClosing Event in the API
+             * 
+             * Closing the second form wouldn't exit the application.So, we needed to force an exit while it
+             * was closing, Microsoft ([s.a.]) demonstrates how to access this moment using the 
+             * Form.FormClosing Event in the API.
+             * 
+             * In order to use Application.Exit() a method of sorts that folows FormClosingEventHandler
+             * Delegate needs to be made in order to add it to the forms Form.FormClosing Event, Microsoft
+             * ([s.a.]) demonstrate how to do this in their API.
+             * 
+             * In the YouTube video Code Monkey explains how delegates work and how to use them. He also
+             * explains how to use a lambda expression, which is what I used (see What are Delegates? (C#
+             * Basics, Lambda, Action, Func), 2020).
              */
-            this.FormClosing += TurnPlayerForm_FormClosing;
-        }
-
-        /* 
-         * Note: This is an in-text citation (for url see the reference list in References.Docx)
-         * 
-         * In order to use Application.Exit() a method that folows FormClosingEventHandler Delegate needs to be made in order to add it to the forms Form.FormClosing Event, Microsoft ([s.a.]) demonstrate how to do this in their API
-         */
-        private void TurnPlayerForm_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            Application.Exit();
+            this.FormClosing += (object sender, FormClosingEventArgs e) =>
+            {
+                Application.Exit();
+            };
         }
 
         private void btnAttack_Click(object sender, EventArgs e)
