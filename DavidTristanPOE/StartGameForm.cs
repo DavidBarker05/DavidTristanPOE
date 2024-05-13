401152966b7fcd2ab5e89523a88c85df1cc3c26d
DavidBarker05 <davidbarker9611@gmail.com>
2024-05-13 11:28:54 +0200

    Added comments to SGF

--- DavidTristanPOE/StartGameForm.cs
+++ DavidTristanPOE/StartGameForm.cs
@@ -8,23 +8,29 @@ namespace DavidTristanPOE
     public partial class StartGameForm : Form
     {
 
-        private string[] p1Data, p2Data;
-        private int[] p1Values, p2Values;
+        private string[] p1Data, p2Data; // Array for storing player details(data)
+        private int[] p1Values, p2Values; // Aray for storing the dragon's statistics
 
+        // Fire Dragon stats
         private const string FIRE_DRAG_NAME = "Fire Dragon";
         private const int FIRE_DRAG_HP = 20, FIRE_DRAG_ATK = 5, FIRE_DRAG_SPATK = 12, FIRE_DRAG_BLOCK = 4;
 
+        // Ice Dragon stats
         private const string ICE_DRAG_NAME = "Ice Dragon";
         private const int ICE_DRAG_HP = 30, ICE_DRAG_ATK = 4, ICE_DRAG_SPATK = 9, ICE_DRAG_BLOCK = 5;
 
+        // Wind Dragon stats
         private const string WIND_DRAG_NAME = "Wind Dragon";
         private const int WIND_DRAG_HP = 40, WIND_DRAG_ATK = 3, WIND_DRAG_SPATK = 7, WIND_DRAG_BLOCK = 5;
 
+        // Earth Dragon stats
         private const string EARTH_DRAG_NAME = "Earth Dragon";
         private const int EARTH_DRAG_HP = 50, EARTH_DRAG_ATK = 2, EARTH_DRAG_SPATK = 5, EARTH_DRAG_BLOCK = 6;
 
+        // Bools to check if both players have saved so that btnStartGame will work when pressed
         private bool player1Saved = false, player2Saved = false;
 
+        // Colours used when changing colours in StartGameForm
         private static readonly Color white = Color.White;
         private static readonly Color black = Color.Black;
         private static readonly Color green = Color.Green;
@@ -38,10 +44,12 @@ namespace DavidTristanPOE
         private static readonly Color saddleBrown = Color.SaddleBrown;
         private static readonly Color limeGreen = Color.LimeGreen;
 
+        // Dragon images
         private static readonly Image fireDragImg = Properties.Resources.Fire_Dragon;
         private static readonly Image iceDragImg = Properties.Resources.Ice_Dragon;
         private static readonly Image windDragImg = Properties.Resources.Wind_Dragon;
         private static readonly Image earthDragImg = Properties.Resources.Earth_Dragon;
+        // Dictionary lookup to get the dragon image according to its type
         private static readonly Dictionary<string, Image> images = new Dictionary<string, Image>()
         {
             { FIRE_DRAG_NAME, fireDragImg },
@@ -50,6 +58,7 @@ namespace DavidTristanPOE
             { EARTH_DRAG_NAME, earthDragImg }
         };
 
+        // Dictionary lookup to get the dragon's values according to its type
         private static readonly Dictionary<string, int[]> values = new Dictionary<string, int[]>()
         {
             { FIRE_DRAG_NAME, new int[4] { FIRE_DRAG_HP, FIRE_DRAG_ATK, FIRE_DRAG_SPATK, FIRE_DRAG_BLOCK } },
@@ -61,8 +70,8 @@ namespace DavidTristanPOE
         public StartGameForm()
         {
             InitializeComponent();
-            rbtnFireDragon1.Select();
-            rbtnFireDragon2.Select();
+            rbtnFireDragon1.Select(); // Select Fire Dragon by default for p1
+            rbtnFireDragon2.Select(); // Select Fire Dragon by default for p2
         }
 
         private void BtnSave1_Click(object sender, EventArgs e)
@@ -84,57 +93,57 @@ namespace DavidTristanPOE
                 int[] vals = values[type];
                 SaveValues(playerNumber, playerName, dragonName, type, vals[0], vals[1], vals[2], vals[3]);
                 playerSaved = true;
-                if (opponentSaved) UpdateColor(white, black, btnStartGame);
-                UpdateColor(green, black, pnlPlayer, lblPlayer);
-                UpdateColor(lime, black, btnSave);
+                if (opponentSaved) UpdateColor(white, black, btnStartGame); // Update colours of components
+                UpdateColor(green, black, pnlPlayer, lblPlayer); // Update colours of components
+                UpdateColor(lime, black, btnSave); // Update colours of btnSave
             }
         }
 
         private void RbtnFireDragon1_CheckedChanged(object sender, EventArgs e)
         {
-            UpdateColor(red, yellow, pnlDragonType1, lblDragonType1, rbtnFireDragon1, rbtnIceDragon1, rbtnWindDragon1, rbtnEarthDragon1);
+            UpdateColor(red, yellow, pnlDragonType1, lblDragonType1, rbtnFireDragon1, rbtnIceDragon1, rbtnWindDragon1, rbtnEarthDragon1); // Update colours of components
             picDragon1.Image = images[FIRE_DRAG_NAME];
         }
 
         private void RbtnIceDragon1_CheckedChanged(object sender, EventArgs e)
         {
-            UpdateColor(cyan, blue, pnlDragonType1, lblDragonType1, rbtnFireDragon1, rbtnIceDragon1, rbtnWindDragon1, rbtnEarthDragon1);
+            UpdateColor(cyan, blue, pnlDragonType1, lblDragonType1, rbtnFireDragon1, rbtnIceDragon1, rbtnWindDragon1, rbtnEarthDragon1); // Update colours of components
             picDragon1.Image = images[ICE_DRAG_NAME];
         }
 
         private void RbtnWindDragon1_CheckedChanged(object sender, EventArgs e)
         {
-            UpdateColor(silver, darkSlateGray, pnlDragonType1, lblDragonType1, rbtnFireDragon1, rbtnIceDragon1, rbtnWindDragon1, rbtnEarthDragon1);
+            UpdateColor(silver, darkSlateGray, pnlDragonType1, lblDragonType1, rbtnFireDragon1, rbtnIceDragon1, rbtnWindDragon1, rbtnEarthDragon1); // Update colours of components
             picDragon1.Image = images[WIND_DRAG_NAME];
         }
 
         private void RbtnEarthDragon1_CheckedChanged(object sender, EventArgs e)
         {
-            UpdateColor(saddleBrown, limeGreen, pnlDragonType1, lblDragonType1, rbtnFireDragon1, rbtnIceDragon1, rbtnWindDragon1, rbtnEarthDragon1);
+            UpdateColor(saddleBrown, limeGreen, pnlDragonType1, lblDragonType1, rbtnFireDragon1, rbtnIceDragon1, rbtnWindDragon1, rbtnEarthDragon1); // Update colours of components
             picDragon1.Image = images[EARTH_DRAG_NAME];
         }
 
         private void RbtnFireDragon2_CheckedChanged(object sender, EventArgs e)
         {
-            UpdateColor(red, yellow, pnlDragonType2, lblDragonType2, rbtnFireDragon2, rbtnIceDragon2, rbtnWindDragon2, rbtnEarthDragon2);
+            UpdateColor(red, yellow, pnlDragonType2, lblDragonType2, rbtnFireDragon2, rbtnIceDragon2, rbtnWindDragon2, rbtnEarthDragon2); // Update colours of components
             picDragon2.Image = images[FIRE_DRAG_NAME];
         }
 
         private void RbtnIceDragon2_CheckedChanged(object sender, EventArgs e)
         {
-            UpdateColor(cyan, blue, pnlDragonType2, lblDragonType2, rbtnFireDragon2, rbtnIceDragon2, rbtnWindDragon2, rbtnEarthDragon2);
+            UpdateColor(cyan, blue, pnlDragonType2, lblDragonType2, rbtnFireDragon2, rbtnIceDragon2, rbtnWindDragon2, rbtnEarthDragon2); // Update colours of components
             picDragon2.Image = images[ICE_DRAG_NAME];
         }
 
         private void RbtnWindDragon2_CheckedChanged(object sender, EventArgs e)
         {
-            UpdateColor(silver, darkSlateGray, pnlDragonType2, lblDragonType2, rbtnFireDragon2, rbtnIceDragon2, rbtnWindDragon2, rbtnEarthDragon2);
+            UpdateColor(silver, darkSlateGray, pnlDragonType2, lblDragonType2, rbtnFireDragon2, rbtnIceDragon2, rbtnWindDragon2, rbtnEarthDragon2); // Update colours of components
             picDragon2.Image = images[WIND_DRAG_NAME];
         }
 
         private void RbtnEarthDragon2_CheckedChanged(object sender, EventArgs e)
         {
-            UpdateColor(saddleBrown, limeGreen, pnlDragonType2, lblDragonType2, rbtnFireDragon2, rbtnIceDragon2, rbtnWindDragon2, rbtnEarthDragon2);
+            UpdateColor(saddleBrown, limeGreen, pnlDragonType2, lblDragonType2, rbtnFireDragon2, rbtnIceDragon2, rbtnWindDragon2, rbtnEarthDragon2); // Update colours of components
             picDragon2.Image = images[EARTH_DRAG_NAME];
         }
 
@@ -151,6 +160,7 @@ namespace DavidTristanPOE
             }
         }
 
+        // Save the values from input into the arrays
         private void SaveValues(int playerNum, string playerName, string dragonName, string dragonType, int dragHp, int dragAtk, int dragSpatk, int dragBlock)
         {
             if (playerNum == 1)
@@ -165,6 +175,7 @@ namespace DavidTristanPOE
             }
         }
 
+        // Method used to update the background and foreground colours of gui components
         private void UpdateColor(Color backColor, Color foreColor, params Control[] guiComponents)
         {
             foreach (Control guiComponent in guiComponents)
